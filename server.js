0045a8474ae1664d7ed925dc3ff7cd6fb7094e1f
rmawani <riz@digital-minds.ca>
2017-01-15 23:01:48 -0700

    Updated 1 File

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
 
 // use JWT auth to secure the api
-app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate']})); //, '/api/users/register'] }));
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
 
 // routes
 app.use('/login', require('./controllers/login.controller'));
